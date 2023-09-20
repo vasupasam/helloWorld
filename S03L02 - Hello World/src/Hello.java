1071e4787c4af6aaa40e4d3001f2187f7ea7f57d
vasup <vasup@VasuDeva.attlocal.net>
2023-09-20 10:30:20 -0400

    First Commit

---
+++ S03L02 - Hello World/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		String name = "vasu";
+	  System.out.println(name);
+
+	}
+
+}
---
+++ S03L02 - Hello World/src/org/studyeasy/exercise/Exercise.java
@@ -0,0 +1,16 @@
+package org.studyeasy.exercise;
+
+public class Exercise {
+	public static void main(String[] args) {
+
+	for(int i=1; i <=5; i++) {
+        for(int j=1;j<=i;j++){
+            System.out.println('@');
+        }
+
+        System.out.println();
+
+    }
+}
+
+}
